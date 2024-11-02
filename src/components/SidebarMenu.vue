22d6b0681a86ffde235ed5e7512279048e32071a
Duy Hung <nduyhung232@gmail.com>
2024-11-02 15:21:06 +0700

    update sidebar

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -7,11 +7,28 @@
       <li>
         <router-link to="/">Home</router-link>
       </li>
-      <li>
-        <router-link to="/training">Training</router-link>
+      <li @click="toggleSubmenu('threshold')" class="menu-item">
+        <span>Threshold</span>
+        <span class="arrow" :class="{ open: isSubmenuOpen.training }">&#10095;</span>
+        <ul v-if="isSubmenuOpen.threshold" class="submenu">
+          <li><router-link to="/training/basics">Otsu</router-link></li>
+        </ul>
       </li>
-      <li>
-        <router-link to="/predict">Predict</router-link>
+      <li @click="toggleSubmenu('training')" class="menu-item">
+        <span>Training</span>
+        <span class="arrow" :class="{ open: isSubmenuOpen.training }">&#10095;</span>
+        <ul v-if="isSubmenuOpen.training" class="submenu">
+          <li><router-link to="/training/basics">Basics</router-link></li>
+          <li><router-link to="/training/advanced">Advanced</router-link></li>
+        </ul>
+      </li>
+      <li @click="toggleSubmenu('predict')" class="menu-item">
+        <span>Predict</span>
+        <span class="arrow" :class="{ open: isSubmenuOpen.predict }">&#10095;</span>
+        <ul v-if="isSubmenuOpen.predict" class="submenu">
+          <li><router-link to="/predict/classification">Classification</router-link></li>
+          <li><router-link to="/predict/regression">Regression</router-link></li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -24,6 +41,24 @@ export default {
     isOpen: {
       type: Boolean,
       required: true
+    }
+  },
+  data() {
+    return {
+      isSubmenuOpen: {
+        threshold: false,
+        training: false,
+        predict: false
+      }
+    };
+  },
+  methods: {
+    toggleSubmenu(menu) {
+      this.isSubmenuOpen = {
+        training: false,
+        predict: false,
+        [menu]: !this.isSubmenuOpen[menu]
+      };
     }
   }
 };
@@ -45,17 +80,6 @@ export default {
   left: 0;
 }
 
-.sidebar .close-btn {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: none;
-  border: none;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-}
-
 .sidebar ul {
   list-style-type: none;
   padding: 0;
@@ -65,9 +89,11 @@ export default {
   padding: 12px 16px;
   font-size: 15px;
   text-align: center;
+  cursor: pointer;
+  position: relative;
 }
 
-.sidebar ul li a {
+.sidebar ul li {
   color: white;
   text-decoration: none;
   display: block;
@@ -75,5 +101,27 @@ export default {
 
 .sidebar ul li a:hover {
   color: #76ABAE;
+}
+
+/* Mũi tên */
+.menu-item .arrow {
+  position: absolute;
+  right: 20px;
+  transition: transform 0.3s;
+}
+
+.menu-item .arrow.open {
+  transform: rotate(90deg); /* Xoay mũi tên sang phải khi mở submenu */
+}
+
+.submenu {
+  list-style-type: none;
+  padding: 0;
+  margin-left: 15px;
+}
+
+.submenu li {
+  padding: 8px 16px;
+  font-size: 14px;
 }
 </style>
